ee65a1f9c869b57dc204fd6f123283bd1e2d8d98
Gareth Simpson <gareth.simpson@zoodigital.com>
2019-05-31 20:50:21 +0100

    staticfiles

--- feedthing/settings.py
+++ feedthing/settings.py
@@ -59,7 +59,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, "_staticfiles")
+
+STATIC_ROOT = getattr(settings_server, "STATIC_ROOT", os.path.join(SITE_ROOT, "static"))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
